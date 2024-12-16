5212044241cfc4db26686399404936440d005b28
Pedro Geadas <pedro.geadas@seqera.io>
2024-12-16 00:18:34 +0000

    Added Day15 part1

--- Advent2024/Day15.fs
+++ Advent2024/Day15.fs
@@ -1,2 +1,125 @@
 module Advent2024.Day15
 
+open Advent2024.Common
+open Advent2024.Matrix
+
+let toDirection direction =
+    match direction with
+    | '^' -> Up
+    | '>' -> Right
+    | '<' -> Left
+    | 'v' -> Down
+    | _ -> failwith "Invalid direction"
+
+let groupAndCount (input: char array) =
+    input
+    |> Array.fold
+        (fun acc c ->
+            let direction = toDirection c
+
+            match acc with
+            | (prevChar, count) :: tail when prevChar = direction -> (prevChar, count + 1) :: tail
+            | _ -> (direction, 1) :: acc)
+        []
+    |> List.rev
+
+let tryFindNextEmptyPosition
+    (matrix: ResizeArray<ResizeArray<char>>)
+    (direction: StandardDirection)
+    (currentPosition: int * int)
+    =
+    let rec searchInDirection pos =
+        let nextX, nextY = nextPositionStandard pos direction
+
+        match matrix[nextX][nextY] with
+        | '.' -> Some(nextX, nextY)
+        | '#' -> None
+        | _ -> searchInDirection (nextX, nextY)
+
+    searchInDirection currentPosition
+
+let print (matrix: ResizeArray<ResizeArray<char>>) =
+    for row in matrix do
+        for char in row do
+            printf "%c" char
+
+        printfn ""
+
+    printfn ""
+
+
+let rec move (currentPosition: int * int) (matrix: ResizeArray<ResizeArray<char>>) (moves: ResizeArray<StandardDirection * int>) =
+
+    if (moves.Count = 0) then
+        matrix
+    else
+        // printfn "Current position: %A" (moves.Item(0))
+        // print matrix
+
+        let currentDirection, currentSteps = Seq.head moves
+        let x, y = nextPositionStandard currentPosition currentDirection
+
+        if matrix[x][y] = '#' then
+            moves.RemoveAt(0)
+            move currentPosition matrix moves
+        else if matrix[x][y] = '.' then
+            moves[0] <- (currentDirection, currentSteps - 1)
+            matrix[x][y] <- '@'
+            matrix[fst currentPosition][snd currentPosition] <- '.'
+
+            if (snd moves[0] = 0) then
+                moves.RemoveAt(0)
+
+            move (x, y) matrix moves
+        else
+            let nextPosition = tryFindNextEmptyPosition matrix currentDirection currentPosition
+
+            match nextPosition with
+            | Some(nextX, nextY) ->
+                matrix[nextX][nextY] <- 'O'
+                matrix[x][y] <- '.'
+                matrix[fst currentPosition][snd currentPosition] <- '.'
+                moves[0] <- (currentDirection, currentSteps - 1)
+
+                if (snd moves[0] = 0) then
+                    moves.RemoveAt(0)
+
+                move (x, y) matrix moves
+            | None ->
+                moves.RemoveAt(0)
+                move currentPosition matrix moves
+
+let gpsValue (matrix: ResizeArray<ResizeArray<char>>) =
+    seq {
+        for i in 0 .. matrix.Count - 1 do
+            for j in 0 .. matrix[i].Count - 1 do
+                if matrix[i][j] = 'O' then
+                    yield (i, j)
+    }
+    |> Seq.map (fun (i, j) -> i * 100 + j)
+    |> Seq.sum
+
+let toResizeArray2d (matrix: char array list) =
+    let array2d = ResizeArray<ResizeArray<char>>()
+
+    for i in 0 .. matrix.Length - 1 do
+        let row = ResizeArray<char>()
+
+        for j in 0 .. matrix[i].Length - 1 do
+            row.Add(matrix[i][j])
+
+        array2d.Add(row)
+
+    array2d
+
+let part1 () =
+    let (matrix, startPosition), list =
+        readMatrixAndGroups "/Users/pgeadas/RiderProjects/Advent2024/Advent2024/inputs/Day15.txt" '@'
+
+    let groups = groupAndCount ((List.head list) |> Array.concat)
+
+    match startPosition with
+    | Some startPosition ->
+        let matrix = move startPosition (toResizeArray2d matrix) (ResizeArray(groups))
+        gpsValue matrix
+    | None -> failwith "No start position found"
--- Advent2024/Main.fs
+++ Advent2024/Main.fs
@@ -3,59 +3,64 @@
 module Advent2024.Main
 
 let runDay1 () =
-    printfn "\nDay 1:"
-    printfn "Part 1: %A" (Day1.part1 ())
-    printfn "Part 2: %A" (Day1.part2 ())
+    printfn "Day 1:"
+    printfn "Part 1: \n%A" (Day1.part1 ())
+    printfn "Part 2: \n%A" (Day1.part2 ())
 
 let runDay2 () =
-    printfn "\nDay 2:"
-    printfn "Part 1: %A" (Day2.part1 ())
-    printfn "Part 2: %A" (Day2.part2 ())
+    printfn "Day 2:"
+    printfn "Part 1: \n%A" (Day2.part1 ())
+    printfn "Part 2: \n%A" (Day2.part2 ())
 
 let runDay3 () =
-    printfn "\nDay 3:"
-    printfn "Part 1: %A" (Day3.part1 ())
-    printfn "Part 2: %A" (Day3.part2 ())
+    printfn "Day 3:"
+    printfn "Part 1: \n%A" (Day3.part1 ())
+    printfn "Part 2: \n%A" (Day3.part2 ())
 
 let runDay4 () =
-    printfn "\nDay 4:"
-    printfn "Part 1: %A" (Day4.part1 ())
-    printfn "Part 2: %A" (Day4.part2 ())
+    printfn "Day 4:"
+    printfn "Part 1: \n%A" (Day4.part1 ())
+    printfn "Part 2: \n%A" (Day4.part2 ())
 
 // Day5 was not a good day. I took too long trying solving it using graphs.
 // For part2, tried using topological sort when it was not needed, and it didn't work.
 // Ended up solving it in Kotlin instead using simple sorting.
 let runDay5 () =
-    printfn "\nDay 5:"
-    printfn "Part 1: %A" (Day5.part1 ())
+    printfn "Day 5:"
+    printfn "Part 1: \n%A" (Day5.part1 ())
 
 let runDay6 () =
-    printfn "\nDay 6:"
-    printfn "Part 1: %A" (Day6.part1 ())
+    printfn "Day 6:"
+    printfn "Part 1: \n%A" (Day6.part1 ())
 
 let runDay7 () =
-    printfn "\nDay 7:"
-    printfn "Part 1: %A" (Day7.part1 ())
-    printfn "Part 2: %A" (Day7.part2 ())
+    printfn "Day 7:"
+    printfn "Part 1: \n%A" (Day7.part1 ())
+    printfn "Part 2: \n%A" (Day7.part2 ())
 
 let runDay9 () =
-    printfn "\nDay 9:"
-    printfn "Part 1: %A" (Day9.part1 ())
-    printfn "Part 2: %A" (Day9.part2 ())
+    printfn "Day 9:"
+    printfn "Part 1: \n%A" (Day9.part1 ())
+    printfn "Part 2: \n%A" (Day9.part2 ())
 
 let runDay10 () =
-    printfn "\nDay 10:"
-    printfn "Part 1: %A" (Day10.part1 ())
-    printfn "Part 2: %A" (Day10.part2 ())
+    printfn "Day 10:"
+    printfn "Part 1: \n%A" (Day10.part1 ())
+    printfn "Part 2: \n%A" (Day10.part2 ())
 
 let runDay13 () =
-    printfn "\nDay 13:"
-    printfn "Part 1: %A" (Day13.part1 ())
+    printfn "Day 13:"
+    printfn "Part 1: \n%A" (Day13.part1 ())
 
 let runDay14 () =
-    printfn "\nDay 14:"
-    printfn "Part 1: %A" (Day14.part1 ())
-    printfn "Part 2: %A" (Day14.part2 ())
+    printfn "Day 14:"
+    printfn "Part 1: \n%A" (Day14.part1 ())
+    printfn "Part 2: \n%A" (Day14.part2 ())
+
+let runDay15 () =
+    printfn "Day 15:"
+    printfn "Part 1: \n%A" (Day15.part1 ())
+    //printfn "Part 2: \n%A" (Day15.part2 ())
 
 [<EntryPoint>]
 let main argv =
@@ -74,6 +79,7 @@ let main argv =
         | 10 -> runDay10 ()
         | 13 -> runDay13 ()
         | 14 -> runDay14 ()
+        | 15 -> runDay15 ()
         | n -> printfn "Day %d not implemented" n
     | _ -> printfn "Usage: program.exe [day]"
 
